9c2ac1fd6bc42244b898b20b2875da00988a7a8d
RyanTW97 <briiancueva97@gmail.com>
2025-03-08 05:31:04 -0500

    Corrige variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["strapi-s3-tricolor.s3.us-east-2.amazonaws.com"],
-    unoptimized: true,
   },
-  output: "standalone", // 🚀 Asegura que el proyecto se ejecute como standalone
 };
 
-export default nextConfig;
+module.exports = nextConfig;
