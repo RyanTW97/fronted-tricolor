9929a5ca34a027a489d65a8bae6d0bcbc609387d
RyanTW97 <briiancueva97@gmail.com>
2025-03-08 04:05:10 -0500

    Primer commit - frontend listo para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ["strapi-s3-tricolor.s3.us-east-2.amazonaws.com"],
-  },
-};
-
-export default nextConfig;
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "package.json",
+      "use": "@vercel/next"
+    }
+  ]
+}
